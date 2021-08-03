83b2f57b06f1756aa3aff1fe5804e77990f4d82e
bardrabbit <bardrabbit@gmail.com>
2021-08-02 21:06:08 -0400

    set up app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+
+  const handlePlayerChoice = (choice) => {
+    setPlayerChoice(choice);
+    getComputerChoice();
+  };
+
+  const getComputerChoice = () => {
+    let random = Math.random() * 3;
+    let index = Math.floor(random);
+    let comp = choices[index];
+    setComputerChoice(comp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Rock Paper Scissors</h2>
+      <div className="choices">
+        <img
+          onClick={() => {
+            handlePlayerChoice('PAPER');
+          }}
+          src="./images/Paper.png"
+          alt="paper"
+          width={120}
+        />
+        <img
+          onClick={() => {
+            handlePlayerChoice('ROCK');
+          }}
+          src="./images/Rock.png"
+          alt="rock"
+          width={120}
+        />
+        <img
+          onClick={() => {
+            handlePlayerChoice('SCISSORS');
+          }}
+          src="./images/Scissors.png"
+          alt="scissors"
+          width={120}
+        />
+      </div>
     </div>
   );
 }
